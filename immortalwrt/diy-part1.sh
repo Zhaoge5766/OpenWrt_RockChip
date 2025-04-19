f5e1a9d93d3889482792acfb7fdbddd0eda5d889
Zhaoge5766 <61320924+Zhaoge5766@users.noreply.github.com>
2025-04-19 17:01:44 +0800

    Update diy-part1.sh

--- immortalwrt/diy-part1.sh
+++ immortalwrt/diy-part1.sh
@@ -32,5 +32,8 @@ git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/l
 rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+# 添加插件
+rm -rf package/luci-app-amlogic
+git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 
 merge_package https://github.com/DHDAXCW/lede-rockchip lede-rockchip/package/wwan
